4e28125069b5878ee44b9e9bcfc75eb39bfabb22
Hamilkar247 <adonnibal96@gmail.com>
2021-08-24 15:52:15 +0200

    [#3] generator paczek danych od arka - z wartościami !
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

---
+++ sql_app/generator_danych/generator_paczek_danych_od_arka.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from random import randrange
+import json
+
+
+def generate_json_paczka():
+    numery_seryjny = ["AXZFS213", "FWQ1000", "QW320D"]
+    sn = numery_seryjny[randrange(0, 3)]
+    a = randrange(0, 9)
+    b = randrange(0, 9)
+    c = randrange(0, 9)
+    z = randrange(0, 9)
+    kod = "0000000"
+
+    value = {
+        "sn": sn,
+        "a": a,
+        "b": b,
+        "c": c,
+        "z": z,
+        "kod": kod
+    }
+
+    return json.dumps(value)
+
+
+print(generate_json_paczka())
