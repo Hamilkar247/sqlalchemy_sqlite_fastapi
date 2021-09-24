6c8a082e12d03cffcec9a6774014ccc1d173344c
Hamilkar247 <adonnibal96@gmail.com>
2021-09-24 11:55:28 +0200

    [#32] uzycie bazy mysql
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,17 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import event
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://db_user:db_pass@localhost/db_name"
+
 
 ##check_same_thread - tylko dla sqlite
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL # SQLITE:, connect_args={"check_same_thread": False}
 )
-#dla sqlite3 wymagane by dzialal cascade_delete
-event.listen(engine, 'connect', lambda c, _: c.execute('pragma foreign_keys=on'))
 
+#dla sqlite3 wymagane by dzialal cascade_delete
+#event.listen(engine, 'connect', lambda c, _: c.execute('pragma foreign_keys=on'))
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
 Base = declarative_base()
+
